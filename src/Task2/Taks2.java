337a1f5ecff8be884dd8122b7c401f160e61f2a1
shubham <shubhammistry239@gmail.com>
2022-07-08 12:44:26 +0530

    Task done

---
+++ src/Task2/Taks2.java
@@ -0,0 +1,8 @@
+package Task2;
+
+public class Taks2 {
+
+	public static void main(String[] args) {
+		System.out.println("hello ");
+	}
+}
---
+++ src/Task4/Task4.java
@@ -0,0 +1,8 @@
+package Task4;
+
+public class Task4 {
+	public static void main(String[] args) {
+		System.out.println("Hello world to task 4 ");
+	}
+
+}
